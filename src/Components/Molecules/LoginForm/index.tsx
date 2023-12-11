834753fbd0cb56dafbefb5ddb3c4c2e739576456
Jake <jake_lewis12@hotmail.co.uk>
2023-12-11 11:10:00 +0000

    used input atom props

--- src/Components/Molecules/LoginForm/index.tsx
+++ src/Components/Molecules/LoginForm/index.tsx
@@ -5,7 +5,8 @@ function LoginForm()
 {
    return (
        <>
-           <InputAtom />
+           <InputAtom label='Email:' />
+           <InputAtom label='Password:' />
            <Button value='Login'/>
        </>
    )
