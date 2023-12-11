f4afd9932bee5c7e08bd4d5d9a7cc378de213eca
Tiago <tiagovarela01@hotmail.com>
2023-12-11 12:17:35 +0000

    changes made to database

--- DB/noodles.sql
+++ DB/noodles.sql
@@ -2,26 +2,34 @@ CREATE DATABASE IF NOT EXISTS noodler;
 
 USE noodler;
 
-DROP TABLE IF EXISTS `users_table`;
+DROP TABLE IF EXISTS `users`;
 
-DROP TABLE IF EXISTS `noodles_table`;
+DROP TABLE IF EXISTS `noodles`;
 
-CREATE TABLE `users_table` (
-                               `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
-                               `username` varchar(255),
-                               `description` varchar(500),
-                               `email` varchar(255),
-                               `password` varchar(255),
-                               PRIMARY KEY(`id`)
+CREATE TABLE `users` (
+                         `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+                         `username` varchar(255) NOT NULL,
+                         `description` varchar(500),
+                         `email` varchar(255) NOT NULL,
+                         `password` varchar(255) NOT NULL,
+                         PRIMARY KEY(`id`)
+);
+INSERT INTO `users` (`id`, `username`, `description`, `email`, `password`) VALUES (1, 'tiago', 'i like noodles', 'tiago@noodles.com', 'ilikenoodles');
+
+INSERT INTO `users` (`id`, `username`, `description`, `email`, `password`) VALUES (2, 'Neil', 'noodles are the best', 'neil@noodles.com', 'noodlesforever');
+
+INSERT INTO `users` (`id`, `username`, `description`, `email`, `password`) VALUES (3, 'Andre', 'noodles are my favorite food', 'andre@noodles.com', 'noodlesforlive');
+
+
+
+CREATE TABLE `noodles` (
+                           `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+                           `user_id`int(11) unsigned NOT NULL,
+                           `time` TIMESTAMP NOT NULL,
+                           `noodle` VARCHAR(500),
+                           PRIMARY KEY(`id`),
+                           CONSTRAINT `fk_users` FOREIGN KEY (user_id) REFERENCES `users`(`id`)
+
 );
 
-CREATE TABLE `noodles_table` (
-                                 `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
-                                 `user_id`int(11) unsigned NOT NULL,
-                                 `date` DATE NOT NULL,
-                                 `time` TIMESTAMP NOT NULL,
-                                 `noodle` VARCHAR(500),
-                                 PRIMARY KEY(`id`),
-                                 CONSTRAINT `fk_users_table` FOREIGN KEY (user_id) REFERENCES `users_table`(`id`)
-
-);
+INSERT INTO `noodles` (`user_id`, `time`, `noodle`) VALUES (1, '2023-11-11', 'noodles are the best');
