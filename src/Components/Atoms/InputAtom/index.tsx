eeaa60e90afbf6e1bd07fadad65ec9adea7fb981
Claudia Lim <climweiching@gmail.com>
2023-12-11 10:50:41 +0000

    Input atom component

---
+++ src/Components/Atoms/InputAtom/index.tsx
@@ -0,0 +1,10 @@
+function InputAtom (props) {
+    return (
+        <>
+            <label>{props.label}</label>
+            <input type='text'></input>
+        </>
+    )
+}
+
+export default InputAtom
